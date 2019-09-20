7aa30dd7d16e88c743c479b129f554966e6d6cd2
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-20 18:21:32 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -11,3 +11,11 @@ end
 
 collection = (1..100).to_a
 my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
+
+# def my_find(collection)
+#   i = 0
+#   while i < 100
+#     yield(1)
+#       i = i + 1
+#   end
+# end
