c2c198c22503c11797b0638cbd094caba3a326b9
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-20 18:14:56 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,6 +3,7 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length #iterate thru each ele in passed in collection
+    yield(collection[i]) #for each ele in collection, yield to block
     i += 1
   end
   binding.pry
