550a3f893f1188f8fbc1b8de9163d83d5a8e7296
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-20 18:14:00 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 def my_find(collection)
   i = 0 
-  while i < collection.length
-  i += 1
+  while i < collection.length #iterate thru each ele in passed in collection
+    i += 1
+  end
   binding.pry
 end
