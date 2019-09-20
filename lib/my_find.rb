7a9f5e046dda4d229a2f4a55b091bd3cf1d78439
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-20 18:23:35 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 def my_find(collection)
   i = 0 
-  while i < collection.length #iterate thru each ele in passed in collection
-    yield(collection[i]) #for each ele in collection, yield to block
+  while i < collection.length #Step 1: iterate thru each ele in passed in collection
+    yield(collection[i]) #Step 2: for each ele in collection, yield to block
     i += 1
   end
   binding.pry
